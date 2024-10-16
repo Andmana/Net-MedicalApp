96c66764d800619d96341abebe3246f02878bb6b
andmana <iam.andmana@gmail.com>
2024-10-16 18:38:16 +0700

    Add view model paginatedList

---
+++ Med-341A/Med-341A.viewmodels/VPaginatedList.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Med_341A.viewmodels
+{
+    public class VPaginatedList<T> : List<T>
+    {
+        public int PageIndex { get; set; }
+        public int TotalPages { get; set; }
+        public int TotalData { get; set; }
+
+        public VPaginatedList(List<T> items, int count, int pageIndex, int pageSize)
+        {
+            this.PageIndex = pageIndex;
+            this.TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+            this.TotalData = count;
+
+            this.AddRange(items);
+        }
+
+        public bool HasPreviousPage => PageIndex > 1;
+        public bool HasNextPage => PageIndex < TotalPages;
+        public static VPaginatedList<T> CreateAsync(List<T> source, int pageIndex, int pageSize)
+        {
+            var count = source.Count;
+            var items = source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
+            return new VPaginatedList<T>(items, count, pageIndex, pageSize);
+        }
+    }
+}
