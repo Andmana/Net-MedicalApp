4cf5cb7fa051a4a463244fef2bd2067b97625a7a
andmana <iam.andmana@gmail.com>
2024-10-16 14:18:20 +0700

    add MRole API

---
+++ Med-341A/Med-341A.api/Controllers/apiMRoleController.cs
@@ -0,0 +1,25 @@
+﻿using Med_341A.datamodels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Med_341A.api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class apiMRoleController : ControllerBase
+    {
+        private readonly Med341aContext db;
+
+        public apiMRoleController(Med341aContext _db)
+        {
+            db = _db;
+        }
+
+        [HttpGet("GetAll")]
+        public List<MRole> GetAll()
+        {
+            List<MRole> data = db.MRoles.ToList();
+            return data;
+        }
+    }
+}
