fd574254a2d289de1e85c1e1c821467845d7afc9
fadhilansyah25 <fadhilansyah25@gmail.com>
2024-10-16 15:10:24 +0700

    tambah: VMUser

---
+++ Med-341A/Med-341A.viewmodels/VMUser.cs
@@ -0,0 +1,48 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace Med_341A.viewmodels
+{
+    public class VMUser
+    {
+        public long Id { get; set; }
+
+        public long? BiodataId { get; set; }
+
+        // start: dari tabel biodata
+        public string? Fullname { get; set; }
+
+        public string? MobilePhone { get; set; }
+
+        public byte[]? Image { get; set; }
+
+        public string? ImagePath { get; set; }
+        // end: dari tabel biodata
+
+        public long? RoleId { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Password { get; set; }
+
+        public int? LoginAttempt { get; set; }
+
+        public bool? IsLocked { get; set; }
+
+        public DateTime? LastLogin { get; set; }
+
+        public long CreatedBy { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public long? ModifiedBy { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public long? DeletedBy { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+
+        public bool IsDelete { get; set; }
+    }
+}
