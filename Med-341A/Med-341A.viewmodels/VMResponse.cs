26acfc470e53382ea100fdd237164d71a032712b
fadhilansyah25 <fadhilansyah25@gmail.com>
2024-10-16 14:26:09 +0700

    add: menambahkan class viewmodel VMResponse

---
+++ Med-341A/Med-341A.viewmodels/VMResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace Med_341A.viewmodels
+{
+    public class VMResponse
+    {
+        public bool Success { get; set; }
+
+        public string Message { get; set; }
+
+        public object Entity { get; set; }
+    }
+}
