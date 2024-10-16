a739d3c1d267d4ec30d023b1f991ef462bd5bd15
fadhilansyah25 <fadhilansyah25@gmail.com>
2024-10-16 15:46:54 +0700

    update VMResponse

--- Med-341A/Med-341A.viewmodels/VMResponse.cs
+++ Med-341A/Med-341A.viewmodels/VMResponse.cs
@@ -1,11 +1,15 @@
 ﻿namespace Med_341A.viewmodels
 {
+
     public class VMResponse
     {
+        public VMResponse()
+        {
+            Success = true;
+        }
+
         public bool Success { get; set; }
-
         public string Message { get; set; }
-
         public object Entity { get; set; }
     }
 }
