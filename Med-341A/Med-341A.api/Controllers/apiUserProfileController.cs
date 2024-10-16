e8a6241e895cfe186d67320f750f5d31e2108a6b
novendraar <novendraar@gmail.com>
2024-10-16 18:00:36 +0700

    Update UserProfilController dan Href Header View Profile

---
+++ Med-341A/Med-341A.api/Controllers/apiUserProfileController.cs
@@ -0,0 +1,45 @@
+﻿using Med_341A.datamodels;
+using Med_341A.viewmodels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Drawing;
+using System.Threading.Tasks.Dataflow;
+
+namespace Med_341A.api.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class apiUserProfileController : ControllerBase
+    {
+        private readonly Med341aContext db;
+        private VMResponse respon = new VMResponse();
+        public apiUserProfileController(Med341aContext db)
+        {
+            this.db = db;
+        }
+
+        //Mendapatkan semua data akun dan biodata user berdasarkan id user
+        [HttpGet("GetDataUser/{id}")]
+        public VMUser GetDataUser(int id)
+        {
+            VMUser data = (from u in db.MUsers
+                           join b in db.MBiodata
+                           on u.BiodataId equals b.Id
+                           join c in db.MCustomers
+                           on b.Id equals c.BiodataId
+                           where u.Id == id && u.IsDelete == false
+                           select new VMUser
+                           {
+                               Id = u.Id,
+                               Fullname = b.Fullname,
+                               MobilePhone = b.MobilePhone,
+                               ImagePath = b.ImagePath,
+                               Dob = c.Dob,
+                               Email = u.Email,
+                               Password = u.Password
+
+                           }).FirstOrDefault()!;
+            return data;
+        }
+    }
+}
---
+++ Med-341A/Med-341A/Controllers/UserProfilController.cs
@@ -0,0 +1,22 @@
+﻿using Med_341A.Services;
+using Med_341A.viewmodels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Med_341A.Controllers
+{
+    public class UserProfilController : Controller
+    {
+        private UserProfileService userProfileService;
+
+        public UserProfilController(UserProfileService userProfileService)
+        {
+            this.userProfileService = userProfileService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            VMUser data = await userProfileService.GetDataUser(1); //HardCore
+            return View(data);
+        }
+    }
+}
