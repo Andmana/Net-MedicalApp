cbe350100d6dd2dc660b65c1c7469ad24e78109f
andmana <iam.andmana@gmail.com>
2024-10-17 15:03:12 +0700

    View only reset password

---
+++ Med-341A/Med-341A/Controllers/AuthResetPWController.cs
@@ -0,0 +1,42 @@
+﻿using Med_341A.datamodels;
+using Med_341A.Services;
+using Med_341A.viewmodels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Med_341A.Controllers
+{
+    public class AuthResetPWController : Controller
+    {
+        private readonly AuthService authService;
+        private VMResponse response = new();
+
+        public AuthResetPWController(AuthService authService)
+        {
+            this.authService = authService;
+        }
+
+        // Email Form
+        public IActionResult InputEmail()
+        {
+            return PartialView();
+        }
+
+        public IActionResult InputOTP()
+        {
+            return PartialView();
+        }
+
+        [HttpGet]
+        public async Task<JsonResult> CheckEmailIsRegistered(string email)
+        {
+            var data = await authService.CheckEmailIsRegistered(email);
+            if (data)
+            {
+                HttpContext.Session.SetString("email", email);
+            }
+
+            return Json(data);
+        }
+
+    }
+}
